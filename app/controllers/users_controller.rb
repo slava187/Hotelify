fc5b4f82da1de73956f5f3f2000edf97871c606e
slava187 <viacheslavusov87@gmail.com>
2021-01-25 17:43:53 -0600

    set authorization for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,12 +17,19 @@ class UsersController < ApplicationController
 
     def show
         @user = User.includes(hotels: :ratings).find_by_id(params[:id])
-        redirect_to '/' if !@user
+        redirect_if_not_authorized
+        redirect_to user_path(@user) if !@user
     end
 
     private
 
     def user_params
         params.require(:user).permit(:name, :email, :password)
+    end
+
+    def redirect_if_not_authorized
+        if !@user || @user != current_user
+            render :invalid
+        end
     end
 end
